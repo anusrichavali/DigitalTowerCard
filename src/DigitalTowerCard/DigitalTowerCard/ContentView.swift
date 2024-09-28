241a60443f0710b534b32159f0481d3c96f83e46
Anusri Chavali <chavali.anusri@gmail.com>
2024-09-27 18:11:55 -0700

    initial commit

---
+++ src/DigitalTowerCard/DigitalTowerCard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DigitalTowerCard
+//
+//  Created by Anusri Chavali on 9/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ src/DigitalTowerCard/DigitalTowerCard/DigitalTowerCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalTowerCardApp.swift
+//  DigitalTowerCard
+//
+//  Created by Anusri Chavali on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct DigitalTowerCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
